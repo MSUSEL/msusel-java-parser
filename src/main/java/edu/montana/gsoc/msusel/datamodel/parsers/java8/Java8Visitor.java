ab1dbabab4d2ce649310c44fde982acd1c307121
Isaac Griffith <isaacgriffith@gmail.com>
2020-06-04 11:29:01 -0600

    updated

--- src/main/java/edu/montana/gsoc/msusel/datamodel/parsers/java8/Java8Visitor.java
+++ src/main/java/edu/montana/gsoc/msusel/datamodel/parsers/java8/Java8Visitor.java
@@ -36,6 +36,7 @@ import org.antlr.v4.runtime.tree.ParseTreeVisitor;
  * @param <T>
  *            The return type of the visit operation. Use {@link Void} for
  *            operations with no return type.
+ * @author Isaac Griffith
  */
 public interface Java8Visitor<T> extends ParseTreeVisitor<T> {
 
