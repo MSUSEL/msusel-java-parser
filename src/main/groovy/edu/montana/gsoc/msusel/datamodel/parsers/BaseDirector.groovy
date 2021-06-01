79445a53908fd7577080bf29a0c8871f3f15ea99
Isaac Griffith <isaacgriffith@gmail.com>
2021-06-01 17:58:42 -0600

    updated

--- src/main/groovy/edu/montana/gsoc/msusel/datamodel/parsers/BaseDirector.groovy
+++ src/main/groovy/edu/montana/gsoc/msusel/datamodel/parsers/BaseDirector.groovy
@@ -81,9 +81,14 @@ abstract class BaseDirector {
         logger.atInfo().log("Processing files and their contained info")
 
         logger.atInfo().log("Gathering File and Type Info into Model")
-        GParsExecutorsPool.withPool(4) {
+        GParsPool.withPool(4) {
 //        files.each { File file -> if (includeFile(file)) gatherFileAndTypeInfo(file) }
-            files.eachParallel { File file -> if (includeFile(file)) gatherFileAndTypeInfo(file) }
+            files.eachParallel { File file ->
+                if (includeFile(file)) {
+                    println file
+                    gatherFileAndTypeInfo(file)
+                }
+            }
         }
 
         logger.atInfo().log("Gathering Type Members and Basic Relation Info into Model")
